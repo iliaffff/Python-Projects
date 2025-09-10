6b88628a3a9d82aafdafd741fce447a4abbf2032
Ilia Fallah <iliafallah138413@gmail.com>
2025-09-10 14:29:04 +0330

    Update

--- Web Scraping For News Websites/main.py
+++ Web Scraping For News Websites/main.py
@@ -2,73 +2,46 @@ import requests
 import bs4
 from pathlib import Path
 import time
-from khayyam import JalaliDatetime
+import jdatetime
 
-currentDate = JalaliDatetime.now().date()
+currentDate = jdatetime.date.today()
 
 varzesh3_url = "https://www.varzesh3.com/news"
-tasnim_url = "https://www.tasnimnews.com/"
-mehr_url = "https://www.mehrnews.com/"
+
 
 # Varzesh 3
 def varzesh3():
+    #Making Folders
     Path("Web Scraping For News Websites/varzesh3").mkdir(exist_ok=True)
-    Path(f"Web Scraping For News Websites/varzesh3/{currentDate}").mkdir(exist_ok=True)
+    Path(f"Web Scraping For News Websites/varzesh3/{str(currentDate)}").mkdir(exist_ok=True)
     response = requests.get(varzesh3_url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     htmlText = open("file.txt", "a", encoding="utf-8")
-    htmlText.write(soup.prettify())
+    htmlText.write(soup.prettify()) # makes a human_readable form of the html text with indentation
+    #Finding all the titles of the news
     titleName = soup.find_all("span", attrs={"class": "v31h7i4cw v31ua5tub v3104kibb v3fifm61 v317e3ifc v3a7kkou v3117nqv4 v3at24cr v3b3r6kr v3lyipyv v3wc7x28"})
-    textFile = open(f"Web Scraping For News Websites/varzesh3/{str(currentDate)}/{titleName[0].text}", "a", encoding="utf-8")
-
-    for i in soup.find_all("a", href=True):
-        href = i['href']
-        if href.startswith("http") and titleName[0].text.split()[0] in href:
-            var1 = requests.get(href)
-            soup1 = bs4.BeautifulSoup(var1.text, "html.parser")
-            newsText = soup1.find_all("p", attrs={"style": "text-align:justify"})
-            for item in newsText:
-                textFile.write(f"{item.text}\n\n")
-            break
+    for j in range(len(titleName)):
+        fileName = titleName[j].text.replace(" ", "_")
+        fileName = titleName[j].text.replace(":", "_")    
+    textFile = open(f"Web Scraping For News Websites/varzesh3/{str(currentDate)}/{fileName}.txt", "a", encoding="utf-8")
+    
+    #Finding the first http link
+    iterator = 0
+    while iterator < len(titleName):
+        for i in soup.find_all("a", href=True):
+            href = i['href']
+            if href.startswith("http") and titleName[iterator].text.split()[0] in href:
+                #Saving the text of the first news
+                var1 = requests.get(href)
+                soup1 = bs4.BeautifulSoup(var1.text, "html.parser")
+                newsText = soup1.find_all("p", attrs={"style": "text-align:justify"})
+                for item in newsText:
+                    textFile.write(f"{item.text}\n\n")
+            iterator += 1
     textFile.close()
     
-    
-
-    
-    # linkList = soup.find("a", attrs={"href"})
-    # linkList = open(f"") 
-    # for i in range(len(titleName)):
-    #     textFile = open(f"Web Scraping For News Websites/varzesh3/{currentDate}/{titleName[i].text}", "a", encoding="utf-8")
-    # newsText = soup.find_all("p", attrs={"style": "text-align:justify"})
-    # for news in newsText:
-    #     textFile.write(f"{news.text}\n\n")
-    # textFile.close()
-    
-# def tasnim():
-#     Path("Web Scraping For News Websites/tasnim").mkdir(exist_ok=True)
-#     response = requests.get(tasnim_url)
-#     soup = bs4.BeautifulSoup(response.text, "html.parser")
-#     newsList = soup.find_all("h4", attrs={"class": "lead"})
-#     Path(f"Web Scraping For News Websites/varzesh3/{currentDate}").mkdir(exist_ok=True)    
-#     for news in newsList:
-#         textFile.write(f"{news.text}\n\n")
-#     textFile.close()
-
-# def mehrNews():
-#     Path("Web Scraping For News Websites/mehrnews").mkdir(exist_ok=True)
-#     response = requests.get(mehr_url)
-#     soup = bs4.BeautifulSoup(response.text, "html.parser")
-#     newsList = soup.find_all("p")
-#     Path(f"Web Scraping For News Websites/varzesh3/{currentDate}").mkdir(exist_ok=True)    
-#     for news in newsList:
-#         if "All" not in news.text:
-#             textFile.write(f"{news.text}\n\n")
-#     textFile.close()
-
-
 varzesh3()
-# tasnim()
-# mehrNews()
+
 
 
 
